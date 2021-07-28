0c4bdca7e23799c4037c5d9daee74d0410b77192
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2021-07-28 14:43:41 -0300

    add apps to project settings

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -3,6 +3,9 @@ from .views import PostList, PostDetail
 
 app_name = 'blog_api'
 
+# endpoints
+# 1 to show and individual post
+# 2 to show all posts
 urlpatterns = [
     path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
     path('', PostList.as_view(), name='listcreate'),
--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = [
+    'blog',
+    'blog_api',
+]
+
+INSTALLED_APPS = BASE_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
