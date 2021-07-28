94b9a92f939fb6098432cd5979f297c09ea3cc77
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2021-07-28 17:21:53 -0300

    update settings for postgres database and migrate

--- core/settings.py
+++ core/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgresdjangorest',
+        'USER': 'jazminbernal',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
